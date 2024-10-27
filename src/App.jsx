fd6e4c481e236dff15898c118098c4f04181a9be
Stephan Model <stephan.model@outlook.de>
2024-10-27 17:30:07 +0100

    add essential sections as JSX to App for homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import linkedInIcon from "./assets/linkedIn.svg";
+import githubIcon from "./assets/github.svg";
+import mailLogo from "./assets/mail.svg";
+
+import viteLogo from "/vite.svg";
+import portrait from "./assets/images/portrait.jpeg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <img src={portrait} alt="portrait" width={460} height={328} />
+      <p>
+        conceptual<span> / </span>
+        interdisciplinary<span> / </span>
+        agile
       </p>
+      <h1>Stephan Model</h1>
+      <h2>Front End Developer</h2>
+      <h3>About</h3>
+      <p>Bullet</p>
+      <p>Bullet</p>
+      <p>Bullet</p>
+
+      <h3>Let's connect</h3>
+      <a href="https://vite.dev" target="_blank">
+        <img src={linkedInIcon} className="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vite.dev" target="_blank">
+        <img src={githubIcon} className="logo" alt="Vite logo" />
+      </a>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
