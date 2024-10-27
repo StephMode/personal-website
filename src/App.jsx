d987a6b759c482c6b16a36b07aa115a234cbfdcc
Stephan Model <stephan.model@outlook.de>
2024-10-27 17:48:03 +0100

    Add content to about me section

--- src/App.jsx
+++ src/App.jsx
@@ -19,18 +19,35 @@ function App() {
       </p>
       <h1>Stephan Model</h1>
       <h2>Front End Developer</h2>
-      <h3>About</h3>
-      <p>Bullet</p>
-      <p>Bullet</p>
-      <p>Bullet</p>
+      <section className="about-me--section">
+        <h3>About</h3>
+        <p>
+          Aspiring Front-End Developer with a background in international sales,
+          project management, and tech partnerships. Currently training in web
+          development at neue fische, where I’m building hands-on projects.
+        </p>
+        <p>
+          In past roles, I led sales and integration projects across Europe,
+          collaborating closely with cross-functional teams. My focus has always
+          been on solving technical challenges with user-centered solutions—this
+          drive inspired me to transition to web development.
+        </p>
+        <p>
+          I’m excited to bring my technical skills and collaborative mindset to
+          a development team, crafting user-friendly interfaces and tackling
+          real-world problems through code.
+        </p>
+      </section>
 
-      <h3>Let's connect</h3>
-      <a href="https://vite.dev" target="_blank">
-        <img src={linkedInIcon} className="logo" alt="Vite logo" />
-      </a>
-      <a href="https://vite.dev" target="_blank">
-        <img src={githubIcon} className="logo" alt="Vite logo" />
-      </a>
+      <section className="connect--section">
+        <h3>Let's connect</h3>
+        <a href="https://vite.dev" target="_blank">
+          <img src={linkedInIcon} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://vite.dev" target="_blank">
+          <img src={githubIcon} className="logo" alt="Vite logo" />
+        </a>
+      </section>
     </>
   );
 }
