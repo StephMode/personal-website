17e6051d8f05e5c1f89f64a81bf091b43fe4ddb3
Stephan Model <stephan.model@outlook.de>
2025-01-30 10:42:50 +0100

    refactor: refine styling of Project related components, add project images, drill props
    decided to go for flexbox only layout for project tiles as it seem to scale better with the many nested elements inside each Project Tile

--- src/components/pages/ProjectsPage.jsx
+++ src/components/pages/ProjectsPage.jsx
@@ -7,12 +7,14 @@ const projects = [
     description:
       "Digital handbook with tips on plants and plant care. Final project of the Web Development Bootcamp. Development in a team of 4 people using agile methodology.",
     technologies: ["React", "StyledComponents"],
+    imageURL: "/images/project-showcase-rooted.png",
   },
   {
     title: "iro",
     description:
       "Single Page Color Palette Application. Project of the Web Development Bootcamp, in which I built a frontend in React for the first time. I am constantly developing the app further. ",
     technologies: ["React", "StyledComponents"],
+    imageURL: "/images/project-showcase-iro.png",
   },
 ];
 
@@ -25,6 +27,7 @@ export default function ProjectsPage() {
             projectTitle={project.title}
             projectDescription={project.description}
             projectTechnologies={project.technologies}
+            projectImage={project.imageURL}
           />
         );
       })}
@@ -33,8 +36,25 @@ export default function ProjectsPage() {
 }
 
 const StyledProjectTileContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, minmax(300px, 1fr));
-  grid-template-rows: repeat(1, minmax(330px, 1fr));
-  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+  justify-content: space-evenly;
+  align-items: flex-start;
+  width: 800px;
+
+  @media screen and (max-width: 840px) {
+  }
 `;
+
+// const StyledProjectTileContainer = styled.div`
+//   display: grid;
+//   grid-template-columns: repeat(2, minmax(400px, 1fr));
+//   grid-template-rows: repeat(1, minmax(450px, 1fr));
+//   border: 5px solid green;
+//   gap: 10px;
+//   @media screen and (max-width: 840px) {
+//     grid-template-columns: repeat(1, minmax(350px, 1fr));
+//     grid-template-rows: repeat(2, minmax(250px, 1fr));
+//   }
+// `;
--- src/components/ui/ProjectTile.jsx
+++ src/components/ui/ProjectTile.jsx
@@ -4,38 +4,60 @@ export default function ProjectTile({
   projectTitle,
   projectDescription,
   projectTechnologies,
+  projectImage,
 }) {
   return (
     <StyledProjectCard>
-      <h2>{projectTitle}</h2>
-      <StyledParagraph>{projectDescription}</StyledParagraph>
-      <ul>
-        {projectTechnologies.map((tech) => {
-          return <li>{tech}</li>;
-        })}
-      </ul>
+      <StyledImage src={projectImage} alt="img" />
+      <StyledProjectCardWrapperInside>
+        <h2>{projectTitle}</h2>
+        <StyledParagraph>{projectDescription}</StyledParagraph>
+        <ul>
+          {projectTechnologies.map((tech) => {
+            return <li>{tech}</li>;
+          })}
+        </ul>
+      </StyledProjectCardWrapperInside>
     </StyledProjectCard>
   );
 }
 
 const StyledProjectCard = styled.div`
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
-  border: 1px solid green;
+  width: 100%;
+  background-color: rgba(200, 200, 2000, 20%);
+  border-radius: 10px;
+`;
+
+const StyledProjectCardWrapperInside = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 4px dotted pink;
+  width: 100%;
+  padding: var(--padding-s);
+  gap: 10px;
 `;
 
 const StyledParagraph = styled.p`
   text-align: left;
-  margin: 00;
-  padding: var(--padding-xs);
+  margin: 0;
   display: block;
   width: 100%;
-  max-width: 200px;
+  /* max-width: 200px; */
   word-wrap: break-word;
   font-size: var(--fontSize-paragraph);
   @media screen and (max-width: 840px) {
     font-size: var(--fontSize-paragraph-mobile);
+  }
+`;
+
+const StyledImage = styled.img`
+  width: 200px;
+  height: 200px;
+  filter: grayscale(1);
+  &:hover {
+    filter: grayscale(0);
   }
 `;
